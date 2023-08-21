0bdc0797562c872a1f29b5e410fd874897a0f280
denpro87 <edmon9907@gmail.com>
2023-08-21 12:09:16 +0400

    init commit

---
+++ src/domain/customer.ts
@@ -0,0 +1,32 @@
+export interface Country {
+  name?: string;
+  code?: string;
+}
+
+export interface Representative {
+  name?: string;
+  image?: string;
+}
+
+export interface Customer {
+  id?: number;
+  name?: string;
+  country?: Country;
+  company?: string;
+  date?: string | Date;
+  status?: string;
+  activity?: number;
+  representative?: Representative;
+  verified?: boolean;
+  balance?: number;
+}
+
+export interface Order {
+  id: string;
+  productCode?: string;
+  date?: string | Date;
+  amount?: number;
+  quantity?: number;
+  seller?: string;
+  status?: string;
+}
